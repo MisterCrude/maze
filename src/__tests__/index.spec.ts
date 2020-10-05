5e9acf929e77df9b86d1a0aa6c59a4a1de486460
Ivyman <calamberia@gmail.com>
2020-10-05 09:52:39 +0200

    test: add tests for utils

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,3 +1,79 @@
-test('basic', () => {
-  expect(0).toBe(0);
+import {
+  getEndPoint,
+  getPoint,
+  isValidPoint,
+  getQueueNode,
+  isValidEndStartPoints
+} from '../utils';
+import { IPoint, IQueueNode } from '../types';
+
+it('gets correct endpoint', () => {
+  const endpoint: IPoint = getEndPoint(10, 5);
+
+  expect(endpoint).toMatchObject<IPoint>({ x: 10 - 1, y: 5 - 2 });
+});
+
+it('gets correct point', () => {
+  const point: IPoint = getPoint(10, 10);
+
+  expect(point).toMatchObject<IPoint>({ x: 10, y: 10 });
+});
+
+it('checks is point valid', () => {
+  const testPointX: number = 0;
+  const testPointY: number = 9;
+  const ROWS: number = 10;
+  const COLS: number = 10;
+
+  const isValid: boolean = isValidPoint(testPointX, testPointY, COLS, ROWS);
+
+  expect(isValid).toBeTruthy();
+});
+
+it('checks is point invalid', () => {
+  const testPointX: number = 0;
+  const testPointY: number = 20;
+  const ROWS: number = 10;
+  const COLS: number = 10;
+
+  const isInvalid: boolean = isValidPoint(testPointX, testPointY, COLS, ROWS);
+
+  expect(isInvalid).toBeFalsy();
+});
+
+it('gets queue node', () => {
+  const point: IPoint = { x: 2, y: 4 };
+  const dist: number = 5;
+
+  const queueNode: IQueueNode = getQueueNode(point, dist);
+
+  expect(queueNode).toMatchObject<IQueueNode>({ point, dist });
+});
+
+it('checks start point or end point is valid', () => {
+  const matrix: string[][] = [
+    ['0', '1', '0'],
+    ['0', '1', '0'],
+    ['0', '1', '1']
+  ];
+  const startPoint: IPoint = { x: 1, y: 0 };
+  const endPoint: IPoint = { x: 1, y: 2 };
+
+  const isValid: boolean = isValidEndStartPoints(matrix, startPoint, endPoint);
+
+  expect(isValid).toBeTruthy();
+});
+
+it('checks star tpoint or end point is invalid', () => {
+  const matrix: string[][] = [
+    ['0', '1', '0'],
+    ['0', '1', '0'],
+    ['0', '1', '0']
+  ];
+  const startPoint: IPoint = { x: 1, y: 0 };
+  const endPoint: IPoint = { x: 0, y: 2 };
+
+  const isValid: boolean = isValidEndStartPoints(matrix, startPoint, endPoint);
+
+  expect(isValid).toBeFalsy();
 });
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,32 @@
+import { IPoint, IQueueNode } from './types';
+
 export const isValideUrl = (input: string): boolean => input?.length < 3;
 
-export const print = (message: any): void => console.log(message);
+export const print = (...args: any): void => console.log(...args);
+
+export const getEndPoint = (cols: number, rows: number): IPoint => ({
+  x: cols - 1,
+  y: rows - 2
+});
+
+export const getPoint = (x: number, y: number): IPoint => ({ x, y });
+
+export const isValidPoint = (
+  row: number,
+  col: number,
+  COLS: number,
+  ROWS: number
+): boolean => row >= 0 && row < ROWS && col >= 0 && col < COLS;
+
+export const getQueueNode = (point: IPoint, dist: number): IQueueNode => ({
+  point,
+  dist
+});
+
+export const isValidEndStartPoints = (
+  matrix: string[][],
+  startPoint: IPoint,
+  endPoint: IPoint
+): boolean =>
+  Number(matrix[startPoint.y][startPoint.x]) === 1 &&
+  Number(matrix[endPoint.y][endPoint.x]) === 1;
