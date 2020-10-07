0d2820ba2dd37d28451041f934fb84cc7d9aeb28
Ivyman <calamberia@gmail.com>
2020-10-07 18:05:12 +0200

    feat: add messages

---
+++ src/messages.ts
@@ -0,0 +1,11 @@
+const messages: Record<string, string> = {
+  urlQuestionMsg: 'Enter URL address to labyrinth file:',
+  urlErrorMsg: 'Please provide a valid URL address',
+  scriptErrorMsg: "Did't find correct destination",
+  scriptSuccessMsg: 'Shortest distance is:',
+  fetchFileErrorMsg: "File can't be reachable",
+  pointInvalidMsg: 'Startpoint or endpoint is invalid',
+  appNameMsg: 'labyrinth'
+};
+
+export default messages;
